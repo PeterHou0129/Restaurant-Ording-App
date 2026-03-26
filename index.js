4fd1dc264fbb950fdb4a466b620658af30b4222a
PeterHou0129 <206045650+PeterHou0129@users.noreply.github.com>
2026-03-27 02:51:15 +0800

    set the orderArr

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 import { menuArray } from "./data.js"
-
-
+ const  orderArr = []
 document.addEventListener('click',function(e){
     if(e.target.dataset.pizza){
-        plusPizaClick(e.target.dataset.pizza)
+        plusClick(e.target.dataset.pizza)
+        renderPrice(e.target.dataset.pizza)
+        
     }
     else if(e.target.dataset.hamburger){
-        plusHamburgerClick(e.target.dataset.hamburger)
+        plusClick(e.target.dataset.hamburger)
+        renderPrice(e.target.dataset.hamburger)
     }
     else if(e.target.dataset.beer){
-        plusBeerClick(e.target.dataset.beer)
+        plusClick(e.target.dataset.beer)
+        renderPrice(e.target.dataset.beer)
     }
 })
 
-function plusPizaClick(id){
-    console.log(id)
+function plusClick(menuId){
+   const targetMenuObj = menuArray.filter(function(item){
+     return item.id.toString() === menuId
+   })[0]
+    
+    document.getElementById('renderPrice').style.display = 'flex'
+   return targetMenuObj.price
 }
 
-function plusHamburgerClick(id){
-      console.log(id)
-}
-
-function plusBeerClick(id){
-      console.log(id)
-}
 
 function getMenuHtml(){
     let menuHtml = ``
+    
     menuArray.map(function(item){
-        const listArr = []
         menuHtml += `
         <div class="menu-inner">
             <img class="icon" src="pic/${item.name}.png" >
@@ -47,14 +48,39 @@ function getMenuHtml(){
         <div class="graylight"></div>
 
         `
+       
     })
 
-    return menuHtml
+    return menuHtml 
+}
+
+function renderPrice(orderItemID){
+   
+    const targetOrderObj = menuArray.filter(function(item){
+     return item.id.toString() === orderItemID
+   })[0]
+   
+   orderArr.push(targetOrderObj)
+   console.log(orderArr)
+
+    // menuArray.map(function(item){
+    //     renderPriceHtml += `
+    //     <div class="renderPrice" id="renderPrice">
+    //         <h1>Your Order</h1>
+    //         <div class="order-item">
+    //             <h3>${item.name}</h3>
+    //             <h4>$${item.price}</h4>
+    //         </div>
+            
+    // </div>`
+    // })
+     
+    return  orderArr
 }
 
 
 function render(){
-    document.getElementById('menu').innerHTML = getMenuHtml()
+    document.getElementById('menu').innerHTML = getMenuHtml() 
 }
 
 render()
